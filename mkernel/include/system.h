553b131a8b436fa5ec81170ba00a187b9daf17e9
No1CharlesWu <No1CharlesWu@163.com>
2016-03-14 15:04:09 +0800

    reset gdt,good

---
+++ mkernel/include/system.h
@@ -0,0 +1,130 @@
+/* bkerndev - Bran's Kernel Development Tutorial
+*  By:   Brandon F. (friesenb@gmail.com)
+*  Desc: Global function declarations and type definitions
+*
+*  Notes: No warranty expressed or implied. Use at own risk. */
+#ifndef __SYSTEM_H
+#define __SYSTEM_H
+
+typedef int size_t;
+
+/* This defines what the stack looks like after an ISR was running */
+struct regs
+{
+    unsigned int gs, fs, es, ds;
+    unsigned int edi, esi, ebp, esp, ebx, edx, ecx, eax;
+    unsigned int int_no, err_code;
+    unsigned int eip, cs, eflags, useresp, ss;    
+};
+
+///* MAIN.C */
+//extern void *memcpy(void *dest, const void *src, size_t count);
+//extern void *memset(void *dest, char val, size_t count);
+//extern unsigned short *memsetw(unsigned short *dest, unsigned short val, size_t count);
+//extern size_t strlen(const char *str);
+//extern unsigned char inportb (unsigned short _port);
+//extern void outportb (unsigned short _port, unsigned char _data);
+//
+///* CONSOLE.C */
+//extern void init_video(void);
+//extern void puts(unsigned char *text);
+//extern void putch(unsigned char c);
+//extern void cls();
+
+/* GDT.C */
+extern void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran);
+extern void gdt_install();
+
+/* IDT.C */
+extern void idt_set_gate(unsigned char num, unsigned long base, unsigned short sel, unsigned char flags);
+extern void idt_install();
+
+/* ISRS.C */
+extern void isrs_install();
+
+/* IRQ.C */
+extern void irq_install_handler(int irq, void (*handler)(struct regs *r));
+extern void irq_uninstall_handler(int irq);
+extern void irq_install();
+
+/* TIMER.C */
+extern void timer_wait(int ticks);
+extern void timer_install();
+
+/* KEYBOARD.C */
+extern void keyboard_install();
+
+/* Defines a GDT entry */
+struct gdt_entry
+{
+    unsigned short limit_low;
+    unsigned short base_low;
+    unsigned char base_middle;
+    unsigned char access;
+    unsigned char granularity;
+    unsigned char base_high;
+} __attribute__((packed));
+
+struct gdt_ptr
+{
+    unsigned short limit;
+    unsigned int base;
+} __attribute__((packed));
+
+/* Our GDT, with 3 entries, and finally our special GDT pointer */
+struct gdt_entry gdt[5];
+struct gdt_ptr gp;
+
+/* This is in start.asm. We use this to properly reload
+*  the new segment registers */
+extern void gdt_flush();
+
+/* Setup a descriptor in the Global Descriptor Table */
+void gdt_set_gate(int num, unsigned long base, unsigned long limit, unsigned char access, unsigned char gran)
+{
+    /* Setup the descriptor base address */
+    gdt[num].base_low = (base & 0xFFFF);
+    gdt[num].base_middle = (base >> 16) & 0xFF;
+    gdt[num].base_high = (base >> 24) & 0xFF;
+
+    /* Setup the descriptor limits */
+    gdt[num].limit_low = (limit & 0xFFFF);
+    gdt[num].granularity = ((limit >> 16) & 0x0F);
+
+    /* Finally, set up the granularity and access flags */
+    gdt[num].granularity |= (gran & 0xF0);
+    gdt[num].access = access;
+}
+
+/* Should be called by main. This will setup the special GDT
+*  pointer, set up the first 3 entries in our GDT, and then
+*  finally call gdt_flush() in our assembler file in order
+*  to tell the processor where the new GDT is and update the
+*  new segment registers */
+void gdt_install()
+{
+    /* Setup the GDT pointer and limit */
+    gp.limit = (sizeof(struct gdt_entry) * 5) - 1;
+    gp.base = &gdt;
+
+    /* Our NULL descriptor */
+    gdt_set_gate(0, 0, 0, 0, 0);
+
+    /* The second entry is our Code Segment. The base address
+    *  is 0, the limit is 4GBytes, it uses 4KByte granularity,
+    *  uses 32-bit opcodes, and is a Code Segment descriptor.
+    *  Please check the table above in the tutorial in order
+    *  to see exactly what each value means */
+    gdt_set_gate(1, 0, 0xFFFFFFFF, 0x9A, 0xCF);
+
+    /* The third entry is our Data Segment. It's EXACTLY the
+    *  same as our code segment, but the descriptor type in
+    *  this entry's access byte says it's a Data Segment */
+    gdt_set_gate(2, 0, 0xFFFFFFFF, 0x92, 0xCF);
+
+    gdt_set_gate(3, 0x000b8000, 0x0000ffff, 0x92, 0xcf);
+    gdt_set_gate(4, 0x00500000, 0x00000fff, 0x92, 0xcf);
+    /* Flush out the old GDT and install the new changes! */
+    gdt_flush();
+}
+#endif
