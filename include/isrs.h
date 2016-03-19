3430d8ef5526ab2fa1ded5c342731d3b2bc0f4f2
No1CharlesWu <No1CharlesWu@163.com>
2016-03-19 14:00:01 +0800

    test for isrs.h isr0-23

--- include/isrs.h
+++ include/isrs.h
@@ -21,14 +21,14 @@ extern void isr12();
 extern void isr13();
 extern void isr14();
 extern void isr15();
-//extern void isr16();
-//extern void isr17();
-//extern void isr18();
-//extern void isr19();
-//extern void isr20();
-//extern void isr21();
-//extern void isr22();
-//extern void isr23();
+extern void isr16();
+extern void isr17();
+extern void isr18();
+extern void isr19();
+extern void isr20();
+extern void isr21();
+extern void isr22();
+extern void isr23();
 //extern void isr24();
 //extern void isr25();
 //extern void isr26();
@@ -67,15 +67,15 @@ void isrs_install()
     idt_set_gate(14, (unsigned)isr14, 0x08, 0x8E);
     idt_set_gate(15, (unsigned)isr15, 0x08, 0x8E);
 
-//    idt_set_gate(16, (unsigned)isr16, 0x08, 0x8E);
-//    idt_set_gate(17, (unsigned)isr17, 0x08, 0x8E);
-//    idt_set_gate(18, (unsigned)isr18, 0x08, 0x8E);
-//    idt_set_gate(19, (unsigned)isr19, 0x08, 0x8E);
-//    idt_set_gate(20, (unsigned)isr20, 0x08, 0x8E);
-//    idt_set_gate(21, (unsigned)isr21, 0x08, 0x8E);
-//    idt_set_gate(22, (unsigned)isr22, 0x08, 0x8E);
-//    idt_set_gate(23, (unsigned)isr23, 0x08, 0x8E);
-//
+    idt_set_gate(16, (unsigned)isr16, 0x08, 0x8E);
+    idt_set_gate(17, (unsigned)isr17, 0x08, 0x8E);
+    idt_set_gate(18, (unsigned)isr18, 0x08, 0x8E);
+    idt_set_gate(19, (unsigned)isr19, 0x08, 0x8E);
+    idt_set_gate(20, (unsigned)isr20, 0x08, 0x8E);
+    idt_set_gate(21, (unsigned)isr21, 0x08, 0x8E);
+    idt_set_gate(22, (unsigned)isr22, 0x08, 0x8E);
+    idt_set_gate(23, (unsigned)isr23, 0x08, 0x8E);
+
 //    idt_set_gate(24, (unsigned)isr24, 0x08, 0x8E);
 //    idt_set_gate(25, (unsigned)isr25, 0x08, 0x8E);
 //    idt_set_gate(26, (unsigned)isr26, 0x08, 0x8E);
--- start.asm
+++ start.asm
@@ -50,6 +50,23 @@ global isr12
 global isr13
 global isr14
 global isr15
+global isr16
+global isr17
+global isr18
+global isr19
+global isr20
+global isr21
+global isr22
+global isr23
+;global isr24
+;global isr25
+;global isr26
+;global isr27
+;global isr28
+;global isr29
+;global isr30
+;global isr31
+
 isr0:
 	cli 
 	push byte 0
@@ -138,6 +155,102 @@ isr15:
     cli
     push byte 0
     push byte 15
+    jmp isr_common_stub
+
+isr16:
+    cli
+    push byte 0
+    push byte 16
+    jmp isr_common_stub
+
+isr17:
+    cli
+    push byte 0
+    push byte 17
+    jmp isr_common_stub
+
+isr18:
+    cli
+    push byte 0
+    push byte 18
+    jmp isr_common_stub
+
+isr19:
+    cli
+    push byte 0
+    push byte 19
+    jmp isr_common_stub
+
+isr20:
+    cli
+    push byte 0
+    push byte 20
+    jmp isr_common_stub
+
+isr21:
+    cli
+    push byte 0
+    push byte 21
+    jmp isr_common_stub
+
+isr22:
+    cli
+    push byte 0
+    push byte 22
+    jmp isr_common_stub
+
+isr23:
+    cli
+    push byte 0
+    push byte 23
+    jmp isr_common_stub
+
+isr24:
+    cli
+    push byte 0
+    push byte 24
+    jmp isr_common_stub
+
+isr25:
+    cli
+    push byte 0
+    push byte 25
+    jmp isr_common_stub
+
+isr26:
+    cli
+    push byte 0
+    push byte 26
+    jmp isr_common_stub
+
+isr27:
+    cli
+    push byte 0
+    push byte 27
+    jmp isr_common_stub
+
+isr28:
+    cli
+    push byte 0
+    push byte 28
+    jmp isr_common_stub
+
+isr29:
+    cli
+    push byte 0
+    push byte 29
+    jmp isr_common_stub
+
+isr30:
+    cli
+    push byte 0
+    push byte 30
+    jmp isr_common_stub
+
+isr31:
+    cli
+    push byte 0
+    push byte 31
     jmp isr_common_stub
 
 extern fault_handler
